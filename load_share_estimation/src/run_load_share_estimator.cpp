057689e8e1f609904bf437b4740f1a3fc08de857
Felix Duvallet <felix.duvallet@epfl.ch>
2016-08-30 11:58:10 +0200

    Use a namespace for the load share estimation.

--- load_share_estimation/src/run_load_share_estimator.cpp
+++ load_share_estimation/src/run_load_share_estimator.cpp
@@ -2,8 +2,11 @@
 #include <load_share_estimation/LoadShareEstimator.h>
 
 int main(int argc, char **argv) {
-  ros::init(argc, argv, "boilerplate_cpp");
-  ros::NodeHandle nodeHandle("load_share_est");
+  ros::init(argc, argv, "load_share_estimator");
+
+  // We initialize the node handle with a namespace, to keep all topics
+  // together.
+  ros::NodeHandle nodeHandle("load_share_estimator");
 
   LoadShareEstimator worker(&nodeHandle);
   if (!worker.init()) {
